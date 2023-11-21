bd26d6f6eecb77faf7aeb702b61fabebec5dbdab
lucas de souza jacinto <lucasdesouzajacinto@gmail.com>
2023-11-21 20:08:09 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let hora = 4;
+
+  function conta(a) {
+    let b = 1;
+    for (let x = 1; x < a; x++) {
+      b *= x;
+    }
+    return b;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá React De novo!</h1>
+      <h2>vou ficar aqui por mais tempo ainda!{conta(hora)}</h2>
     </div>
   );
 }
